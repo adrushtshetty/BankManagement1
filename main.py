1b66fcba2a6352ce5db8e02131a0b36fdc27366d
adrushtshetty <adrushtshetty@gmail.com>
2023-12-10 00:11:33 +0530

    Edit Account Details

--- main.py
+++ main.py
@@ -250,6 +250,7 @@ def chkVerify():
 
 
 
+
         elif (AccountValidity(f,df)) and (AccountValidity(t,df)):
             file1 = open("debug.txt", "w")
             file1.write("Verified both accounts")
@@ -557,6 +558,194 @@ def accunblock():
         return render_template("accountnotfound1.html")
 
     return render_template('unblocksuccess.html')
+
+
+@app.route("/change")
+def change():
+    return render_template("change1.html")
+
+@app.route("/change", methods=["POST"])
+def change1():
+    global accno
+    accno = int(request.form["Acc"])
+    with open('temp.txt', 'w') as file:
+        file.write(f"{accno}")
+    empid = request.form["empid"]
+    pin = request.form["empd"]
+    mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+    cur = mycon.cursor()
+    cur.execute("select account_number from accountdetails")
+    lacc = cur.fetchall()
+    cur.execute("select empid, pin from emp")
+    lemp = cur.fetchall()
+    mycon.commit()
+    cur.close()
+    mycon.close()
+    if (accno,) in lacc and (empid, pin) in lemp:
+        l = ["cno", "em", "add", "acchol", "acctyp", "accstat"]
+        return redirect(url_for(l[int(request.form["sel"])]))
+    else:
+        return redirect(url_for("status", check="InvalidCredentials"))
+
+
+@app.route("/cno")
+def cno():
+    return render_template("Contact_number.html")
+
+
+@app.route("/cno", methods=["POST"])
+def contact():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno = int(accno[0])
+    con = int(request.form["con"])
+    con1 = int(request.form["con1"])
+    if con == con1:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set contact_number={0} where account_number={1}".format(con, accno))
+        mycon.commit()
+        cur.close()
+        mycon.close()
+
+        return redirect(url_for("status", check='success'))
+    else:
+        return redirect(url_for("Statuscno", check='change_error'))
+
+
+@app.route("/statusCn/<check>")
+def Statuscno(check):
+    return render_template("Contact_number.html", check=check)
+
+
+@app.route("/em")
+def em():
+    return render_template("email.html")
+
+
+@app.route("/em", methods=["POST"])
+def email():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno = int(accno[0])
+    email = request.form["email"]
+    email1 = request.form["email1"]
+    if email == email1:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set email=%s where account_number=%s", (email, accno))
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success1'))
+    else:
+        return redirect(url_for("StatusEm", check='change_error'))
+
+
+@app.route("/statusEm/<check>")
+def StatusEm(check):
+    return render_template("email.html", check=check)
+
+
+@app.route("/add")
+def add():
+    return render_template("address.html")
+
+
+@app.route("/add", methods=["POST"])
+def address():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno = int(accno[0])
+    adr = request.form["adr"]
+    if adr != "":
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set address=%s where account_number=%s", (adr, accno))
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success2'))
+    else:
+        return redirect(url_for("StatusAdr", check='change_error'))
+
+
+@app.route("/statusAdr/<check>")
+def StatusAdr(check):
+    return render_template("address.html", check=check)
+
+
+@app.route("/acchol")
+def acchol():
+    return render_template("Account_Holder.html")
+
+
+@app.route("/accounthol", methods=["POST"])
+def accounthol():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno = int(accno[0])
+    ach = request.form["ach"]
+    ach1 = request.form["ach1"]
+    if ach == ach1:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set account_holder=%s where account_number=%s", (ach, accno))
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success3'))
+    else:
+        return redirect(url_for("StatusHol", check='change_error'))
+
+
+@app.route("/statusHol/<check>")
+def StatusHol(check):
+    return render_template("Account_Holder.html", check=check)
+
+
+@app.route("/acctyp")
+def acctyp():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno = int(accno[0])
+    cur.execute("select account_type from accountdetails where account_number=%s", [accno])
+    lacc = cur.fetchall()
+    print(lacc)
+    if ("Current",) in lacc:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set account_type='Savings' where account_number=%s", [accno])
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success4'))
+    else:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set account_type='Current' where account_number=%s", [accno])
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success5'))
+
+@app.route("/status/<check>")
+def status(check):
+    return render_template("change1.html",check=check)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
 
 
--- templates/main.py
+++ templates/main.py
@@ -1,123 +1,179 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, request, jsonify, render_template, url_for,redirect
+import mysql.connector as ms
 import pandas as pd
-import mysql.connector as sqltor
-from processing import *
-from datetime import datetime
+app=Flask(__name__)
+mycon=ms.connect(host="localhost",user="root",passwd="root",database="bank_management")
+cur=mycon.cursor()
 
-
-app = Flask(__name__)
-
-mycon=sqltor.connect(host="localhost",user="root",passwd="admin",database="bank_management")
-
-    # df = pd.read_sql("select * from accountDetails;", mycon)
-    # print(df.head())
-dfa = pd.read_sql(f"select * from accountdetails;", mycon)  # account details table dataframe
-emp = pd.read_sql(f'select * from emp;', mycon)  # employee table dataframe
-date = pd.read_sql(f"select * from stat_acc;", mycon)  # stat_acc table dataframe
-dat = pd.read_sql(f"select * from stat_acc;", mycon)
-mycon.close()
-
-
-
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/status/<check>")
+def status(check):
+    return render_template("change1.html",check=check)
 
-@app.route("/accblock")
-def accblock1():
-    return render_template("form-element.html")
+@app.route("/change")
+def change():
+    return render_template("change1.html")
 
-
-@app.route("/accblock", methods=["POST"])
-def accblock():
-    # f = open("output.txt", "w")
-    a = request.form["accnum"]
-    b = request.form["empid"]
-    c = request.form["emppin"]
-    d = request.form["reason"]
-    e = request.form["date"]
-
-    value = False
-    t1 = False
-    if int(a) in dfa["account_number"].values:
-        value = True
-        if len(dfa["account_status"][findIndex(dfa, "account_status", a)]) == 14:
-            if str(b) in emp["empid"].values and int(emp["pin"][findIndex(emp, "empid", b)]) == int(c):
-                if len(date["date"][findIndex(date, "account_number", a)]) == 11:
-                    t1 = True
-                    dict1 = {"Reason": d, "Blocked": 1}
-                    # to turn mm-dd-yyyy date which is e to dd-mm-yyyy
-                    e = datetime.strptime(e, "%Y-%m-%d").strftime("%d-%m-%Y")
-                    # print(type(d), d, dict1, type(dict1))
-                    mycon=sqltor.connect(host="localhost",user="root",passwd="admin",database="bank_management")
-                    cursor = mycon.cursor()
-                    query = "UPDATE accountDetails SET account_status = %s WHERE account_number = %s"
-                    cursor.execute(query, (str(dict1), a))
-                    quer = "UPDATE stat_acc SET date = %s WHERE account_number = %s"
-                    cursor.execute(quer, (str(e), a))
-                    mycon.commit()
-                    cursor.close()
-                    mycon.close()
-                    # {"Blocked": 0}
-            else:
-                return render_template("employeenotfound.html")
-        else:
-            return render_template("accountalreadyblocked.html", reasonS="{}".format(reason(dfa["account_status"][findIndex(dfa, "account_number", a)])))
+@app.route("/change",methods=["POST"])
+def change1():
+    global accno
+    accno=int(request.form["Acc"])
+    with open('temp.txt', 'w') as file:
+        file.write(f"{accno}")
+    empid=request.form["empid"]
+    pin=request.form["empd"]
+    mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+    cur = mycon.cursor()
+    cur.execute("select account_number from accountdetails")
+    lacc=cur.fetchall()
+    cur.execute("select empid, pin from emp")
+    lemp=cur.fetchall()
+    mycon.commit()
+    cur.close()
+    mycon.close()
+    if (accno,) in lacc and (empid,pin) in lemp:
+        l=["cno","em","add","acchol","acctyp","accstat"]
+        return redirect(url_for(l[int(request.form["sel"])]))
     else:
-        return render_template("accountnotfound.html")
-    # f.write(str(value))
-    # f.write(str(t1))
-    return redirect(url_for("accsucblock"))
+        return redirect(url_for("status",check="InvalidCredentials"))
 
-@app.route("/accsucblock")
-def accsucblock():
-    return render_template("success.html")
+@app.route("/cno")
+def cno():
+    return render_template("Contact number.html")
 
-@app.route("/accunblock")
-def accunblock1():
-    return render_template("unblockform.html")
-
-
-@app.route("/accunblock", methods=["POST"])
-def accunblock():
-    # f = open("output.txt", "w")
-    a = request.form["accnum"]
-    b = request.form["empid"]
-    c = request.form["emppin"]
-    e = request.form["date"]
-
-    value = False
-    t1 = False
-    if int(a) in dfa["account_number"].values:
-        value = True
-        if str(dfa["account_status"][findIndex(dfa, "account_status", a)]) != r"{'Blocked': 0}":
-            if str(b) in emp["empid"].values and int(emp["pin"][findIndex(emp, "empid", b)]) == int(c):
-                if len(dat["date"][findIndex(dat, "account_number", a)]) != 11:
-                    t1 = True
-                    dict1 = {"Blocked": 0}
-                    f = open("output.txt", "w")
-                    f.write(str(dat.columns))
-                    # print(type(d), d, dict1, type(dict1))
-                    mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
-                    cursor = mycon.cursor()
-                    query = "UPDATE accountDetails SET account_status = %s WHERE account_number = %s"
-                    cursor.execute(query, (str(dict1), a))
-                    quer = "UPDATE stat_acc SET date = %s WHERE account_number = %s"
-                    cursor.execute(quer, ("not blocked", a))
-                    mycon.commit()
-                    cursor.close()
-                    mycon.close()
-
-            else:
-                return render_template("employeenotfound1.html")
-        else:
-            return render_template("accountalreadyunblocked.html")
+@app.route("/cno",methods=["POST"])
+def contact():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno=int(accno[0])
+    con=int(request.form["con"])
+    con1=int(request.form["con1"])
+    if con==con1:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set contact_number={0} where account_number={1}".format(con,accno))
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        
+        return redirect(url_for("status",check='success'))
     else:
-        return render_template("accountnotfound1.html")
+        return redirect(url_for("Statuscno",check='change_error'))
 
-    return render_template('unblocksuccess.html')
+@app.route("/statusCn/<check>")
+def Statuscno(check):
+    return render_template("Contact number.html",check=check)
 
+@app.route("/em")
+def em():
+    return render_template("email.html")
 
-if __name__ == '__main__':
+@app.route("/em",methods=["POST"])
+def email():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno=int(accno[0])
+    email = request.form["email"]
+    email1 = request.form["email1"]
+    if email == email1:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set email=%s where account_number=%s",(email, accno))
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success1'))
+    else:
+        return redirect(url_for("StatusEm", check='change_error'))
+
+@app.route("/statusEm/<check>")
+def StatusEm(check):
+    return render_template("email.html",check=check)
+
+@app.route("/add")
+def add():
+    return render_template("address.html")
+
+@app.route("/add",methods=["POST"])
+def address():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno=int(accno[0])
+    adr=request.form["adr"]
+    if adr != "":
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set address=%s where account_number=%s",(adr, accno))
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success2'))
+    else:
+        return redirect(url_for("StatusAdr", check='change_error'))
+
+@app.route("/statusAdr/<check>")
+def StatusAdr(check):
+    return render_template("address.html",check=check)
+
+@app.route("/acchol")
+def acchol():
+    return render_template("Account Holder.html")
+
+@app.route("/accounthol",methods=["POST"])
+def accounthol():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno=int(accno[0])
+    ach = request.form["ach"]
+    ach1 = request.form["ach1"]
+    if ach==ach1:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set account_holder=%s where account_number=%s",(ach, accno))
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success3'))
+    else:
+        return redirect(url_for("StatusHol", check='change_error'))
+
+@app.route("/statusHol/<check>")
+def StatusHol(check):
+    return render_template("Account Holder.html",check=check)
+
+@app.route("/acctyp")
+def acctyp():
+    with open('temp.txt', 'r') as file:
+        content = file.read().splitlines()
+    if len(content) == 1:
+        accno = content
+    accno=int(accno[0])
+    cur.execute("select account_type from accountdetails where account_number=%s",[accno])
+    lacc = cur.fetchall()
+    print(lacc)
+    if ("Current",) in lacc:
+        mycon = sqltor.connect(host="localhost", user="root", passwd="admin", database="bank_management")
+        cur = mycon.cursor()
+        cur.execute("update accountdetails set account_type='Savings' where account_number=%s",[accno])
+        mycon.commit()
+        cur.close()
+        mycon.close()
+        return redirect(url_for("status", check='success4'))
+    else:
+        cur.execute("update accountdetails set account_type='Current' where account_number=%s",[accno])
+        mycon.commit()
+        return redirect(url_for("status", check='success5'))
+
+if __name__ == "__main__":
     app.run(debug=True)
